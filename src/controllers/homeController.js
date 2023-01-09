af5ca29367abf386181cbc4b095af22010171076
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-09 17:33:28 +0700

    CRUD voi Node.js và Sequelize - Su Dung ORM(Edit)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -29,9 +29,32 @@ let displayGetCRUD = async (req, res) => {
     });
 };
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        console.log(userData);
+        return res.render('editCRUD.ejs', {
+            user: userData,
+        });
+    } else {
+        return res.send('Not found');
+    }
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers,
+    });
+};
+
 module.exports = {
     getHomePage,
     getCRUD,
     postCRUD,
     displayGetCRUD,
+    getEditCRUD,
+    putCRUD,
 };
--- src/route/webRoute.js
+++ src/route/webRoute.js
@@ -6,11 +6,14 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/crud', homeController.getCRUD);
+    
     router.get('/get-crud', homeController.displayGetCRUD);
-
     router.post('/post-crud', homeController.postCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD)
 
     return app.use('/', router);
 };
 
 module.exports = initWebRoutes;
+     
