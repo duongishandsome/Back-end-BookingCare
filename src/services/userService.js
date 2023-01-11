2cd96459d5d623d2829da2b2a447f33c625b386d
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-11 17:49:25 +0700

    Cach viet REST APIs

---
+++ src/services/userService.js
@@ -0,0 +1,64 @@
+import db from '../models/index';
+import bcrypt from 'bcryptjs';
+
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let isExist = await checkUserEmail(email);
+            let userData = {};
+
+            if (isExist) {
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
+                    raw: true,
+                });
+
+                if (user) {
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password';
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = 'User not found~';
+                }
+            } else {
+                userData.errCode = 1;
+                userData.errMessage = 'Your email is not exist in system. Plz try other email';
+            }
+
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail },
+            });
+
+            if (user) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+module.exports = {
+    handleUserLogin,
+};
