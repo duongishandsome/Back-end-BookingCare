dd9d2d697e4390005af0a81c7ce70ec1604342c2
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-21 16:38:19 +0700

    Them mot so thu cho react

--- src/services/userService.js
+++ src/services/userService.js
@@ -22,7 +22,7 @@ let handleUserLogin = (email, password) => {
 
             if (isExist) {
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     where: { email: email },
                     raw: true,
                 });
@@ -117,8 +117,9 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phoneNumber,
-                    gender: data.gender === '1' ? true : false,
+                    gender: data.gender,
                     roleId: data.roleId,
+                    positionId: data.positionId,
                 });
                 resolve({
                     errCode: 0,
