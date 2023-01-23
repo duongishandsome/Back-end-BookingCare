51d5f58d2b3e8b9581c405c8fec81cedf6974400
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-24 02:08:37 +0700

    WRITE & READ FILE IMAGE voi Sequelize va MySQL BLOB

--- src/services/userService.js
+++ src/services/userService.js
@@ -120,6 +120,7 @@ let createNewUser = (data) => {
                     gender: data.gender,
                     roleId: data.roleId,
                     positionId: data.positionId,
+                    image: data.avatar,
                 });
                 resolve({
                     errCode: 0,
@@ -161,7 +162,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'missing require parameters',
@@ -176,6 +177,14 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phoneNumber = data.phoneNumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
+
                 await user.save();
 
                 resolve({
