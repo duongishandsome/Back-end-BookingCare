6835ba049c6e39749437fa348ea0869f753a801d
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-27 15:01:59 +0700

    bo sung thong tin cho bac si

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -15,6 +15,35 @@ let getTopDoctorHome = async (req, res) => {
     }
 };
 
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (error) {
+        console.log(error);
+        res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server',
+        });
+    }
+};
+
+let postInfoDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInfoDoctor(req.body);
+
+        return res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server',
+        });
+    }
+};
+
 module.exports = {
     getTopDoctorHome,
+    getAllDoctors,
+    postInfoDoctor,
 };
