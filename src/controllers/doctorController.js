75d810d87594f9a6f74df8f354b8322cd24e623e
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-27 16:17:32 +0700

    Quan He Giua Cac Model & Database

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -42,8 +42,22 @@ let postInfoDoctor = async (req, res) => {
     }
 };
 
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let infor = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(infor);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server',
+        });
+    }
+};
+
 module.exports = {
     getTopDoctorHome,
     getAllDoctors,
     postInfoDoctor,
+    getDetailDoctorById,
 };
