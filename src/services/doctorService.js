8f5fa3e8d26debc603f650f42bfea9f4b52b0109
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-30 00:16:19 +0700

    BulkCreate vs Sequelize

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,4 +1,7 @@
 import db from '../models/index';
+import _ from 'lodash';
+require('dotenv').config();
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
 let getTopDoctorHome = (limit) => {
     return new Promise(async (resolve, reject) => {
@@ -70,10 +73,10 @@ let saveDetailInfoDoctor = (inputData) => {
                         raw: false,
                     });
                     if (doctorMarkdown) {
-                        doctorMarkdown.contentHTML = inputData.contentHTML,
-                        doctorMarkdown.contentMarkdown = inputData.contentMarkdown,
-                        doctorMarkdown.description = inputData.description,
-                        doctorMarkdown.updateAt = new Date()
+                        (doctorMarkdown.contentHTML = inputData.contentHTML),
+                            (doctorMarkdown.contentMarkdown = inputData.contentMarkdown),
+                            (doctorMarkdown.description = inputData.description),
+                            (doctorMarkdown.updateAt = new Date());
                         await doctorMarkdown.save();
                     }
                 }
@@ -128,9 +131,62 @@ let getDetailDoctorById = (inputId) => {
     });
 };
 
+let bulkCreateSchedule = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.arrSchedule || !data.doctorId || !data.formattedDate) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter!',
+                });
+            } else {
+                let schedule = data.arrSchedule;
+                if (schedule && schedule.length > 0) {
+                    schedule = schedule.map((item) => {
+                        item.maxNumber = MAX_NUMBER_SCHEDULE;
+                        return item;
+                    });
+                }
+
+                // get all existing data
+                let existing = await db.Schedule.findAll({
+                    where: { doctorId: data.doctorId, date: data.formattedDate },
+                    attributes: ['timeType', 'date', 'doctorId', 'maxNumber'],
+                    raw: true,
+                });
+
+                // convert date
+                if (existing && existing.length > 0) {
+                    existing = existing.map((item) => {
+                        item.date = new Date(item.date).getTime();
+                        return item;
+                    });
+                }
+
+                // compare different
+                let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+                    return a.timeType === b.timeType && a.date === b.date;
+                });
+
+                // create data
+                if (toCreate && toCreate.length > 0) {
+                    await db.Schedule.bulkCreate(toCreate);
+                }
+                resolve({
+                    errCode: 0,
+                    errMessage: 'OK',
+                });
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
 module.exports = {
     getTopDoctorHome,
     getAllDoctors,
     saveDetailInfoDoctor,
     getDetailDoctorById,
+    bulkCreateSchedule,
 };
