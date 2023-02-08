22f24cbc288f630f8b0f835cf3e6be1b8ded9705
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-02-08 16:04:48 +0700

    Tạo Chức Năng Thêm Chuyên Ngành Khám Bệnh

---
+++ src/services/specialtyService.js
@@ -0,0 +1,32 @@
+import db from '../models/index';
+
+let createSpecialty = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.name || !data.imageBase64 || !data.descriptionMarkdown || !data.descriptionHTML) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter',
+                });
+            } else {
+                await db.Specialty.create({
+                    name: data.name,
+                    image: data.imageBase64,
+                    descriptionHTML: data.descriptionHTML,
+                    descriptionMarkdown: data.descriptionMarkdown,
+                });
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'Ok',
+                });
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+module.exports = {
+    createSpecialty,
+};
