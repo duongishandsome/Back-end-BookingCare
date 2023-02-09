9091465dbc14a9bb93a22f6633738c696eeb094b
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-02-09 22:25:58 +0700

    SpecialtyId vs clinicId

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -51,24 +51,44 @@ let getAllDoctors = () => {
     });
 };
 
+let checkRequiredFields = (inputData) => {
+    let arrFields = [
+        'doctorId',
+        'contentHTML',
+        'contentMarkdown',
+        'action',
+        'selectedPrice',
+        'selectedPayment',
+        'selectedProvince',
+        'nameClinic',
+        'addressClinic',
+        'note',
+        'specialtyId',
+    ];
+
+    let isValid = true;
+    let element = '';
+    for (let i = 0; i < arrFields.length; i++) {
+        if (!inputData[arrFields[i]]) {
+            isValid = false;
+            element = arrFields[i];
+        }
+    }
+
+    return {
+        isValid,
+        element,
+    };
+};
+
 let saveDetailInfoDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (
-                !inputData.doctorId ||
-                !inputData.contentHTML ||
-                !inputData.contentMarkdown ||
-                !inputData.action ||
-                !inputData.selectedPrice ||
-                !inputData.selectedPayment ||
-                !inputData.selectedProvince ||
-                !inputData.nameClinic ||
-                !inputData.addressClinic ||
-                !inputData.note
-            ) {
+            let checkObj = checkRequiredFields(inputData);
+            if (checkObj.isValid === false) {
                 resolve({
                     errCode: 1,
-                    errMessage: 'Missing required parameters',
+                    errMessage: `Missing required parameter: ${checkObj.element}`,
                 });
             } else {
                 // upsert to Markdown
@@ -110,6 +130,8 @@ let saveDetailInfoDoctor = (inputData) => {
                     doctorInfor.nameClinic = inputData.nameClinic;
                     doctorInfor.addressClinic = inputData.addressClinic;
                     doctorInfor.note = inputData.note;
+                    doctorInfor.specialtyId = inputData.specialtyId;
+                    doctorInfor.clinicId = inputData.clinicId;
                     await doctorInfor.save();
                 } else {
                     //create
@@ -121,6 +143,8 @@ let saveDetailInfoDoctor = (inputData) => {
                         nameClinic: inputData.nameClinic,
                         addressClinic: inputData.addressClinic,
                         note: inputData.note,
+                        specialtyId: inputData.specialtyId,
+                        clinicId: inputData.clinicId,
                     });
                 }
                 resolve({
